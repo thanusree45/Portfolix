e1dc83026429e079c70a9d0378f73e9a6249098d
thanusree45 <143024576+thanusree45@users.noreply.github.com>
2024-11-24 11:07:01 +0530

    Update AdminIntro.js

--- client/src/pages/Admin/AdminIntro.js
+++ client/src/pages/Admin/AdminIntro.js
@@ -10,7 +10,7 @@ function AdminIntro() {
    const onFinish = async (values) => {
         try{
             dispatch(ShowLoading());
-            const response = await axios.post('/api/portfolio/update-intro',{
+            const response = await axios.post('https://portfolix.onrender.com/api/portfolio/update-intro',{
                 ...values,
                 _id: portfolioData.intro._id,
             });
